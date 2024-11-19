c39b90a2630777481dc114c51a083ab7180de2d0
EmTampz <mmtampogao@gmail.com>
2024-11-19 08:50:36 +0800

    Add Item

---
+++ frontend/src/components/inventory-form-dialog_addItem.tsx
@@ -0,0 +1,34 @@
+
+import {Plus} from 'lucide-react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import InventoryForm from "./inventory-form_addItem";
+
+export default function InventoryFormDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="flex items-center space-x-2">
+          <Plus className="h-4 w-4" />
+          <span>Add Item</span>
+        </button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add New Item</DialogTitle>
+          <DialogDescription>
+            Enter the details for the new item.
+          </DialogDescription>
+        </DialogHeader>
+        <InventoryForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
