4edcd8d8cb07054c01083fc0ab26ca43d64c0511
wenyngcar <coconut_rain@users.noreply.github.com>
2024-11-21 17:50:21 +0800

    refactor: rename file extension to modular js

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindcssAnimate from 'tailwindcss-animate'
+
 export default {
 	darkMode: ["class"],
 	content: [
@@ -56,5 +59,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 }
