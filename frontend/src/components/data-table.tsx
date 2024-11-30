9d50986a73d3a05fb57546034f6e54db793c483d
wenyngcar <coconut_rain@users.noreply.github.com>
2024-12-01 02:49:50 +0800

    feat: replace table body  with accordion(accordion trigger dummy data)

--- frontend/src/components/data-table.tsx
+++ frontend/src/components/data-table.tsx
@@ -22,18 +22,33 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 import { Input } from "@/components/ui/input";
+import FormDialogAddItem from "./form-dialog-add-item";
+import { Brands } from "./columns";
+import { ChevronDown } from "lucide-react";
 
-interface DataTableProps<TData, TValue> {
+interface BrandDataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }
 
+// interface ItemDataTableProps<TData, TValue> {
+//   itemColumns: ColumnDef<TData, TValue>[];
+//   itemData: TData[];
+// }
+
 export function DataTable<TData, TValue>({
   columns,
   data,
-}: DataTableProps<TData, TValue>) {
+}: BrandDataTableProps<TData, TValue>) {
+  // { itemColumns, itemData }: ItemDataTableProps<TData, TValue>
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
@@ -86,35 +101,50 @@ export function DataTable<TData, TValue>({
               </TableRow>
             ))}
           </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
         </Table>
+        <div>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <div key={row.id} data-state={row.getIsSelected() && "selected"}>
+                {/* {row.getVisibleCells().map((cell) => ( */}
+                <div key={row.id}>
+                  <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem
+                      value={row.id}
+                      className="grid grid-cols-3 px-2 "
+                    >
+                      <AccordionTrigger>
+                        {(row.original as Brands).brand}
+
+                        {/* {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                              )} */}
+                      </AccordionTrigger>
+                      <AccordionTrigger>
+                        {(row.original as Brands).category}
+                        <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+                      </AccordionTrigger>
+                      <FormDialogAddItem
+                        brandId={(row.original as Brands).id}
+                        brandName={(row.original as Brands).brand}
+                        brandCategory={(row.original as Brands).category}
+                      />
+                      <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+                {/* ))} */}
+              </div>
+            ))
+          ) : (
+            <div>
+              <div className="h-24 text-center">No results.</div>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
