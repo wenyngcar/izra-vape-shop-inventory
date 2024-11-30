08db57d06d7fe24aa228a2cf4ae29b9da1db62e2
wenyngcar <coconut_rain@users.noreply.github.com>
2024-12-01 02:48:53 +0800

    feat: fetch item data

--- frontend/src/components/page.tsx
+++ frontend/src/components/page.tsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
-import { Brands, columns } from "./columns";
+import { Brands, Items, columns } from "./columns";
 import { DataTable } from "./data-table";
 import { UseFetchBrands } from "@/hooks/use-fetch-brands";
+import { UseFetchItems } from "@/hooks/use-fetch-items";
 
 export default function DemoPage() {
-  const [data, setData] = useState<Brands[]>([]);
+  const [brandData, setBrandData] = useState<Brands[]>([]);
+  const [itemData, setItemData] = useState<Items[]>([]);
 
+  // Fetching data
   useEffect(() => {
     async function fetchData() {
-      const result = await UseFetchBrands();
-      setData(result);
+      const brandResult = await UseFetchBrands();
+      const itemResult = await UseFetchItems();
+      setBrandData(brandResult);
+      setItemData(itemResult);
     }
     fetchData();
   }, []);
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={brandData} />
     </div>
   );
 }
