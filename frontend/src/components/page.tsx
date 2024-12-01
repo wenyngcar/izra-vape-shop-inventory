9b016190ece0bd6d80a943a0f70d5dcad999da36
wenyngcar <coconut_rain@users.noreply.github.com>
2024-12-01 16:54:30 +0800

    chore: comment out redundant imports, unuse variables

--- frontend/src/components/page.tsx
+++ frontend/src/components/page.tsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
-import { Brands, Items, columns } from "./columns";
+import { Brands, columns } from "./columns";
 import { DataTable } from "./data-table";
 import { UseFetchBrands } from "@/hooks/use-fetch-brands";
-import { UseFetchItems } from "@/hooks/use-fetch-items";
+// import { UseFetchItems } from "@/hooks/use-fetch-items";
 
 export default function DemoPage() {
   const [brandData, setBrandData] = useState<Brands[]>([]);
-  const [itemData, setItemData] = useState<Items[]>([]);
+  // const [itemData, setItemData] = useState<Items[]>([]);
 
   // Fetching data
   useEffect(() => {
     async function fetchData() {
       const brandResult = await UseFetchBrands();
-      const itemResult = await UseFetchItems();
+      // const itemResult = await UseFetchItems();
       setBrandData(brandResult);
-      setItemData(itemResult);
+      // setItemData(itemResult);
     }
     fetchData();
   }, []);
+
+  // console.log(brandData);
 
   return (
     <div className="container mx-auto py-10">
