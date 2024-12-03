9081532cdf7d46c527b92e553398cfe716bb8933
EmTampz <mmtampogao@gmail.com>
2024-12-03 14:57:45 +0800

    Quantity Add and Subtract & Fix return of string

--- frontend/src/components/form-dialog-add-brand.tsx
+++ frontend/src/components/form-dialog-add-brand.tsx
@@ -14,9 +14,9 @@ export default function InventoryFormDialog() {
   return (
     <Dialog>
       <DialogTrigger asChild className="flex items-center">
-        <button className="flex items-center">
-          <Plus className="mr-2 h-4 w-4" />
-          <span>Add Brand</span>
+        <button className="flex items-center bg-black text-white px-6 py-3 rounded-md space-x-3">
+          <Plus className="h-4 w-4" />
+          <span>AddBrand</span>
         </button>
       </DialogTrigger>
       <DialogContent>
