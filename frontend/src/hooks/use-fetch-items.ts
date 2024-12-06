7271452aba9d5a87fc6a9d3351a2ae2eecdf9d7f
wenyngcar <coconut_rain@users.noreply.github.com>
2024-12-06 11:34:47 +0800

    refactor: add brand id to types

--- frontend/src/hooks/use-fetch-items.ts
+++ frontend/src/hooks/use-fetch-items.ts
@@ -5,19 +5,23 @@ import mongoose from "mongoose";
 // Types here must match the field name in collections.
 type Item = {
   _id: mongoose.Types.ObjectId;
+  brandId: mongoose.Types.ObjectId;
   name: string;
   price: number;
   quantity: number;
   expiration: Date;
 };
 
-export async function UseFetchItems(): Promise<Items[]> {
+export async function UseFetchItems(
+  id: mongoose.Types.ObjectId
+): Promise<Items[]> {
   try {
     // Fetch data from your API here.
-    const data = await readProducts({});
+    const data = await readProducts({ brandId: id });
 
     const items = data.map((item: Item) => ({
       id: item._id,
+      brandId: item.brandId,
       name: item.name,
       price: item.price,
       quantity: item.quantity,
