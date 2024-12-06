2a55618c2806281ab62684de798244aed9947704
wenyngcar <coconut_rain@users.noreply.github.com>
2024-12-06 15:54:40 +0800

    refactor: add brand data to useEffect dependency, remove redundant imports

--- frontend/src/components/brand-page.tsx
+++ frontend/src/components/brand-page.tsx
@@ -2,24 +2,18 @@ import { useEffect, useState } from "react";
 import { Brands, columns } from "./columns";
 import { BrandTable } from "./brand-table";
 import { UseFetchBrands } from "@/hooks/use-fetch-brands";
-// import { UseFetchItems } from "@/hooks/use-fetch-items";
 
 export default function BrandPage() {
   const [brandData, setBrandData] = useState<Brands[]>([]);
-  // const [itemData, setItemData] = useState<Items[]>([]);
 
   // Fetching data
   useEffect(() => {
     async function fetchData() {
       const brandResult = await UseFetchBrands();
-      // const itemResult = await UseFetchItems();
       setBrandData(brandResult);
-      // setItemData(itemResult);
     }
     fetchData();
-  }, []);
-
-  // console.log(brandData);
+  }, [brandData]);
 
   return (
     <div className="container mx-auto py-10">
