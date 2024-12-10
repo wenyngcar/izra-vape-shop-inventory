eb943819631d5f42ed7516e3dbb22bed6e51d87a
wenyngcar <owenyoungcarino@gmail.com>
2024-12-10 22:15:42 +0800

    feat: initialization

--- frontend/src/components/delete-single-product.tsx
+++ frontend/src/components/delete-single-product.tsx
@@ -1,6 +1,56 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { Button } from "./ui/button";
 import mongoose from "mongoose";
+import { deleteOneItem } from "@/utils/api";
 
-export default function DeleteProductById(productId: mongoose.Types.ObjectId) {
-  return <Button>Delete</Button>;
+type deleteProduct = {
+  itemId: mongoose.Types.ObjectId;
+};
+
+export default function DeleteItem({ itemId }: deleteProduct) {
+  async function deleteItem() {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    try {
+      deleteOneItem({ productId: itemId });
+    } catch (error) {
+      console.log("There was an error on deleting product", error);
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Delete</Button>
+      </DialogTrigger>
+      <DialogContent onInteractOutside={(event) => event.preventDefault()}>
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to delete this item?</DialogTitle>
+          <DialogDescription>
+            Enter the details for the new item.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button onClick={() => deleteItem()}>Confirm</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
