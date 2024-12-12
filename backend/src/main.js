88bba3a3eb299b3f9272429cafd168c98c032533
kramnev <varecla1@up.edu.ph>
2024-12-12 16:32:23 +0800

    feat: admin achema & model

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,4 +1,3 @@
-
 import path from "path";
 import url from "url";
 
@@ -8,63 +7,74 @@ import mongoose from "mongoose";
 
 import apiRouter from "./middleware/api/router.js";
 import Variant from "./database/models/Variant.js";
+import Admin from "./database/models/Admin.js";
+
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 // This gets the file and folder name of this JavaScript file.
 // This is needed for serving static files.
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = {
-    application: express(),
-    port: process.env.PORT,
+  application: express(),
+  port: process.env.PORT,
 };
 const database = {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    cluster: process.env.DATABASE_CLUSTER,
-    name: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  cluster: process.env.DATABASE_CLUSTER,
+  name: process.env.DATABASE_NAME,
 };
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 const mongodbString = `mongodb+srv://${database.username}:${database.password}@${database.cluster}.n4rnz.mongodb.net/${database.name}?retryWrites=true&w=majority&appName=${database.cluster}`;
 
 // Automatically converts request body to JSON.
 server.application.use(express.json());
 
 // Deals with the CORS policy, that prevents connecting the frontend to the backend.
-server.application.use(cors({
+server.application.use(
+  cors({
     origin: true,
     credentials: true,
-}));
+  })
+);
 
 server.application.use(express.static(path.join(__dirname, "public")));
 server.application.use("/api", apiRouter);
 
 server.application.get("/", (req, res) => {
-    res.redirect("/main");
+  res.redirect("/main");
+});
+server.application.get("/login", (req, res) => {
+  Admin.create(req, body)
+    .then((Admin) => res.json(Admin))
+    .catch((err) => res.json(err));
 });
 
 // Starting the server.
 server.application.listen(server.port, async () => {
-    console.log(`Application is listening on port ${server.port}`);
+  console.log(`Application is listening on port ${server.port}`);
 
-    // Connecting to the MongoDB database.
-    await mongoose.connect(mongodbString, clientOptions);
-    console.log(`Application has successfully connected to MongoDB.`);
+  // Connecting to the MongoDB database.
+  await mongoose.connect(mongodbString, clientOptions);
+  console.log(`Application has successfully connected to MongoDB.`);
 
-    // TODO: Fix this.
-    const closedPodDetails = {
-        name: "Closed Pod",
-        description: "This is a closed pod.",
-        category: "Device",
-    }
+  // TODO: Fix this.
+  const closedPodDetails = {
+    name: "Closed Pod",
+    description: "This is a closed pod.",
+    category: "Device",
+  };
 
-    const variants = await Variant.find(closedPodDetails);
+  const variants = await Variant.find(closedPodDetails);
 
-    if (variants.length === 0) {
-        const variant = new Variant(closedPodDetails);
-        await variant.save();
-    }
+  if (variants.length === 0) {
+    const variant = new Variant(closedPodDetails);
+    await variant.save();
+  }
 });
