df9a7de9d548347a0996f43d01ab3af30c40cbdc
wenyngcar <owenyoungcarino@gmail.com>
2024-12-14 14:30:42 +0800

    feat: add name, category, price, total in schema

--- backend/src/database/schemas/saleSchema.js
+++ backend/src/database/schemas/saleSchema.js
@@ -4,7 +4,11 @@ import mongoose from "mongoose";
 const saleSchema = new mongoose.Schema({
     brandId: mongoose.Types.ObjectId,
     productId: mongoose.Types.ObjectId,
+    name: String,
+    category: String,
     quantity: Number,
+    price: Number,
+    total: Number,
     Date: Date,
 });
 
