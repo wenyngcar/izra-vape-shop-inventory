9ab26f18d062cbf6f05255861d3b1f4bcea339c7
wenyngcar <owenyoungcarino@gmail.com>
2025-01-16 19:35:19 +0800

    feat: add optional filter id

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
+import mongoose from "mongoose";
 
 // Function for fetching data.
-export function getData(url: string) {
-  return axios.get(`http://localhost:3000/api/${url}`);
+// url is required argument, _id is optional argument.
+export function getData(
+  url: string,
+  filter: { _id?: mongoose.Types.ObjectId } = {}
+) {
+  return axios.get(`http://localhost:3000/api/${url}`, {
+    params: filter,
+  });
 }
 
+// Function for deleting data.
 export function deleteData(url: string, id: string) {
   return axios.delete(`http://localhost:3000/api/${url}?_id=${id}`);
 }
