0dff1d347645b8f193d8ca8eefc8fc3dc2671ed4
wenyngcar <owenyoungcarino@gmail.com>
2025-01-17 18:58:02 +0800

    refactor: make base url into variable

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import mongoose from "mongoose";
 
+const baseUrl = "http://localhost:3000/api/";
+
 // Function for fetching data.
 // url is required argument, _id is optional argument.
 export function getData(
   url: string,
   filter: { _id?: mongoose.Types.ObjectId } = {}
 ) {
-  return axios.get(`http://localhost:3000/api/${url}`, {
+  return axios.get(`${baseUrl}${url}`, {
     params: filter,
   });
 }
 
 // Function for deleting data.
 export function deleteData(url: string, id: string) {
-  return axios.delete(`http://localhost:3000/api/${url}?_id=${id}`);
+  return axios.delete(`${baseUrl}${url}?_id=${id}`);
 }
