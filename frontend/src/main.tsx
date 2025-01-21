4f09ec098a0ff164910c558c81e582948ef47635
wenyngcar <owenyoungcarino@gmail.com>
2025-01-21 10:49:29 +0800

    feat: initialize QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+// Create a client
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* Provide Client to the App */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
