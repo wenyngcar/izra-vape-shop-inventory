1fb119aec421c08c903b15b1528f1768fcc2c316
wenyngcar <owenyoungcarino@gmail.com>
2025-01-26 13:54:55 +0800

    feat: use skeleton when pending in brands

--- frontend/src/components/brandComponents/brand-page.tsx
+++ frontend/src/components/brandComponents/brand-page.tsx
@@ -1,6 +1,8 @@
 import useFetchBrand from "@/hooks/useFetchBrands";
 import { columns } from "../columns";
 import { BrandTable } from "./brand-table";
+import { Skeleton } from "@/components/ui/skeleton"
+import { AlertCircle } from "lucide-react";
 
 export default function BrandPage() {
   // Custom hook for fetching brand data
@@ -9,8 +11,20 @@ export default function BrandPage() {
   // If data is still pending. 
   if (isPending) {
     return (
-      <div className="container mx-auto py-10">
-        Loading...
+      <div className="container mx-auto py-10 space-y-8">
+        <div className="grid grid-cols-3 gap-x-5">
+          <Skeleton className="h-10 col-span-2" />
+          <Skeleton className=" h-10" />
+        </div>
+        <div className="space-y-3">
+          <Skeleton className="h-10" />
+          <Skeleton className="h-10" />
+          <Skeleton className="h-10" />
+          <Skeleton className="h-10" />
+          <Skeleton className="h-10" />
+          <Skeleton className="h-10" />
+          <Skeleton className="h-10" />
+        </div>
       </div>)
   }
 
@@ -18,8 +32,17 @@ export default function BrandPage() {
   if (isError) {
     console.error("Error fetching sales:", error);
     return (
-      <div className="container mx-auto py-10">
-        Error loading brand data
+      <div className="container mx-auto py-10 px-10 space-y-4 h-[380px]">
+        <div className="flex space-x-3 text-4xl font-semibold">
+          <AlertCircle color="red" size={43} />
+          <div>
+            Error
+          </div>
+        </div>
+        <ul className="list-disc px-10">
+          <li> Cannot load brand data from the server. </li>
+          <li> Please contact the developers if this error still persist. </li>
+        </ul>
       </div>)
   }
 
