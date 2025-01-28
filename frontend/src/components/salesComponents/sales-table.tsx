335a8bf18f453a0ac927258777ffb567b58b26c9
wenyngcar <owenyoungcarino@gmail.com>
2025-01-28 16:56:12 +0800

    refactor: use tanStackQueryTable for rendering

--- frontend/src/components/salesComponents/sales-table.tsx
+++ frontend/src/components/salesComponents/sales-table.tsx
@@ -9,7 +9,6 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Table,
@@ -19,12 +18,23 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { useToast } from "@/hooks/use-toast";
 import { deleteData } from "@/utils/api";
 import { MongooseId, Sales } from "@/utils/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CircleCheckBig, LucideTrash2 } from "lucide-react";
 import mongoose from "mongoose";
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 // Cannot add icon directly to description. This is needed for the icon
 const ToastWithIcon = () => (
@@ -32,43 +42,64 @@ const ToastWithIcon = () => (
     <CircleCheckBig color="#00f513" />
     <span>Sale deleted successfully</span>
   </div>
-)
+);
 
-export default function SalesTable({ salesData }: { salesData: Sales[] }) {
-  const queryClient = useQueryClient()
-  const { toast } = useToast()
+interface SaleTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export default function SalesTable<TData, TValue>({
+  columns,
+  data,
+}: SaleTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
+  });
+
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
 
   const mutation = useMutation({
     mutationFn: (saleId: MongooseId) => {
       // (1)Argument is url, (2)Argument is the id of the data to be deleted.
-      return deleteData("delete-sales", saleId)
-
-    }, onSuccess: () => {
+      return deleteData("delete-sales", saleId);
+    },
+    onSuccess: () => {
       // Toast is the side notification.
       toast({
         variant: "primary",
         description: <ToastWithIcon />,
-      })
+      });
       // This refetches the item after deleting an sale.
-      queryClient.invalidateQueries({ queryKey: ['sales'] })
-
-    }, onError: (error) => {
+      queryClient.invalidateQueries({ queryKey: ["sales"] });
+    },
+    onError: (error) => {
       // Toast is the side notification.
       toast({
         variant: "destructive",
         title: "Uh oh! Something went wrong.",
         description: "There was a problem with your request.",
-      })
+      });
       console.error("There was an error in deleting sale.", error);
-    }
-  })
+    },
+  });
 
   async function handleDeleteSale(
     saleId: mongoose.Types.ObjectId
   ): Promise<void> {
     try {
       // Method to delete sale.
-      mutation.mutate(saleId)
+      mutation.mutate(saleId);
     } catch (error) {
       console.error("Error deleting sale:", error);
     }
@@ -76,6 +107,16 @@ export default function SalesTable({ salesData }: { salesData: Sales[] }) {
 
   return (
     <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Enter name of product here..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
       <Card>
         <CardHeader>
           <CardTitle>Sales Records</CardTitle>
@@ -83,57 +124,82 @@ export default function SalesTable({ salesData }: { salesData: Sales[] }) {
         <CardContent>
           <Table>
             <TableHeader>
-              <TableRow>
-                <TableHead>Item</TableHead>
-                <TableHead>Category</TableHead>
-                <TableHead>Quantity</TableHead>
-                <TableHead>Price</TableHead>
-                <TableHead>Total</TableHead>
-                <TableHead>Sold At</TableHead>
-                <TableHead>Actions</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {salesData.map((sale, index) => (
-                <TableRow key={index}>
-                  <TableCell>{sale.name}</TableCell>
-                  <TableCell>{sale.category}</TableCell>
-                  <TableCell>{sale.quantity}</TableCell>
-                  <TableCell>&#8369;{sale.price.toFixed(2)}</TableCell>
-                  <TableCell>
-                    &#8369;{(sale.price * sale.quantity).toFixed(2)}
-                  </TableCell>
-                  <TableCell>{new Date(sale.date).toLocaleString()}</TableCell>
-                  <TableCell>
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button variant="outline" size="sm">
-                          <LucideTrash2 size={16} />
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>
-                            Delete Sale Record
-                          </AlertDialogTitle>
-                          <AlertDialogDescription>
-                            Are you sure you want to delete this sale record?
-                            This action cannot be undone.
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Cancel</AlertDialogCancel>
-                          <AlertDialogAction asChild>
-                            <Button onClick={() => handleDeleteSale(sale._id)}>
-                              Delete
-                            </Button>
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
-                  </TableCell>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
               ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                    <TableCell>
+                      <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <Button variant="outline" size="sm">
+                            <LucideTrash2 size={16} />
+                          </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>
+                              Delete Sale Record
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                              Are you sure you want to delete this sale record?
+                              This action cannot be undone.
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction asChild>
+                              <Button
+                                onClick={() =>
+                                  handleDeleteSale((row.original as Sales)._id)
+                                }
+                              >
+                                Delete
+                              </Button>
+                            </AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center text-2xl"
+                  >
+                    No records of sales.
+                  </TableCell>
+                </TableRow>
+              )}
             </TableBody>
           </Table>
         </CardContent>
